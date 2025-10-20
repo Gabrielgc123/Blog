da5ed3a69f26b3343401cf875d6e2ef929484cb4
Gabrielgc123 <gabrielgc12319@gmail.com>
2025-10-20 18:02:27 -0300

    Meu primeiro commit

---
+++ src/main/java/br/edu/infnet/gabrieldealmeidaapi/GabrieldealmeidaapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.gabrieldealmeidaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabrieldealmeidaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabrieldealmeidaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/gabrieldealmeidaapi/GabrieldealmeidaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.gabrieldealmeidaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabrieldealmeidaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
