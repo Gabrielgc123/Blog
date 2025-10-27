8e72272a96c56d2122f2eced64df188258bcb2db
Gabrielgc123 <gabrielgc12319@gmail.com>
2025-10-27 20:21:43 -0300

    Alterando a localização da pasta

---
+++ src/main/java/br/edu/infnet/gabrieldealmeidaapi/model/domain/Post.java
@@ -0,0 +1,14 @@
+package br.edu.infnet.gabrieldealmeidaapi.model.domain;
+
+public class Post {
+
+    int idPost;
+    int idAutor;
+    int idCategoria;
+
+    String titulo;
+    String content;
+    String data;
+
+
+}
